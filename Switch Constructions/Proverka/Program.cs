562a2c9b9fafa6c996e32db7ea81b5b30863b053
Denys Shaitan <schaitan.den@gmail.com>
2025-05-10 17:52:52 +0200

    Proverka 10 may

---
+++ Switch Constructions/Proverka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proverka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World,its proverka!");
+        }
+    }
+}
