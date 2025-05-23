fb0ec93049719606fb4414b55ddccf318f3a454d
Denys Shaitan <schaitan.den@gmail.com>
2025-05-23 19:29:02 +0200

    23 may yoy

--- NotebookForME/Program.cs
+++ NotebookForME/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 
 namespace NotebookForME  
@@ -70,41 +71,98 @@ namespace NotebookForME
         //}
 
 
-        static void Main()
+        //static void Main()
+        //{
+        //    Console.OutputEncoding = Encoding.Unicode;
+
+        //    int[] numbers = { 1, 7, 9, 3, 5 };//Робимо масив
+
+        //    SortArray(numbers);//Визов метода та передача масива методу
+
+        //    Console.WriteLine("Відсортований масив:");
+        //    foreach (int num in numbers)//Цикл проходить по кожному елементу масива 
+        //    {
+        //        Console.WriteLine(num + " ");
+        //    }
+        //}
+        //static void SortArray(int[] array)
+        //{
+        //    int n = array.Length; //Зберігаємо длину в масива в змінну n
+
+        //    for (int i = 0; i < n; i++) //Переміщуємо найбільше число в кінешь
+        //    {
+        //        for (int j = 0; j < n - 1 - i; j++) 
+        //        {
+        //            if (array[j] > array[j + 1])
+        //            {
+        //                int my = array[j];
+        //                array[j] = array[j + 1];
+        //                array[j + 1] = my;   
+        //            }
+        //        }
+        //    }
+        //}
+
+
+
+
+        //static void Main()
+        //{
+        //    int[] numbers = { 5, 2, -3, 16, 0, -90 }; //Робимо масив
+        //    int index; //Індекс числа місце для которого я шукаю
+        //    int currentNumber; //Поточне число
+
+        //    for (int i = 0; i < numbers.Length; i++) //Создаємо цикл фор,создаємо змінну і,масив працює поки не закінчиться,кожну ітерацію збільшуємо на одиницю
+        //    {
+        //        index = i; //Зберігаю індекс числа,котре я буду змінювати
+        //        currentNumber = numbers[i];//Індекс числа місце для которого я шукаю
+        //        while (index > 0 && currentNumber < numbers[index - 1]) //Цикл while з двома умовами,Коментар Довгий,Деніс розкаже сам)
+        //        {
+        //            numbers[index] = numbers[index - 1]; //Беремо число котре зліва,та ставимо його й поклади його на поточне місце
+        //            index--; //Переход на одну позицію зліва
+        //        }
+
+        //        numbers[index] = currentNumber; //Ставимо останне число на його місце
+        //    }
+
+        //    for (int i = 0; i < numbers.Length; i++) //Проходимось по всім елементам масива та виводимо числа на єкран 
+        //    {
+        //        Console.WriteLine(numbers[i]);
+        //    }
+        //}
+
+       static void Main()
         {
-            Console.OutputEncoding = Encoding.Unicode;
+            int[] numbers = { 9,8,7,6,5,4,3,2 };
+            int index;
+            int currentnumber;
 
-            int[] numbers = { 1, 7, 9, 3, 5 };
-
-            SortArray(numbers);
-
-            Console.WriteLine("Відсортований масив:");
-            foreach (int num in numbers)
+            for (int i = 0; i < numbers.Length; i++)
             {
-                Console.WriteLine(num + " ");
-            }
-        }
-         static void SortArray(int[] array)
-        {
-            int n = array.Length; //Зберігаємо длину в масива в змінну n
-
-            for(int i = 0;i < n; i++) 
-            {
-                for (int j = 0;j < n - 1 - i; j++) //Переміщуємо найбільше число в кінешь
+                index = i;
+                currentnumber = numbers[i];
+                while (index > 0 && currentnumber < numbers[index - 1])
                 {
-                    if (array[j] > array[j + 1])
-                    {
-                        int my = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = my;
-                    }
-                } 
+                    numbers[index] = numbers[index - 1];
+                    index--;
+                }
+
+                numbers[index] = currentnumber;
+            }
+
+            for (int i = 0;i < numbers.Length;i++)
+            {
+                Console.WriteLine(numbers[i]);
             }
         }
-            
+      
+        
+
+
+        }
         
     }
 
 
-}
+
 
